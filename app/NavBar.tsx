fdda76971c99041a4a2a52113fbc321d6cfc883c
Mohamed Ali <immoe98@gmail.com>
2024-05-25 22:24:14 +0200

    Add the login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,9 +4,12 @@ import { AiFillBug } from 'react-icons/ai';
 import React from 'react';
 import { usePathname } from 'next/navigation';
 import classNames from 'classnames';
+import { useSession } from 'next-auth/react';
+import { Box } from '@radix-ui/themes';
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' },
@@ -18,19 +21,28 @@ const NavBar = () => {
       </Link>
       <ul className='flex space-x-6'>
         {links.map((link) => (
-          <Link
-            key={link.href}
-            className={classNames({
-              'text-zinc-900': link.href === currentPath,
-              'text-zinc-500': link.href !== currentPath,
-              'hover:text-zinc-800 transition-colors': true,
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              className={classNames({
+                'text-zinc-900': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        {status === 'authenticated' && (
+          <Link href='/api/auth/signout'>Log out</Link>
+        )}
+        {status === 'unauthenticated' && (
+          <Link href='/api/auth/signin'>Log in</Link>
+        )}
+      </Box>
     </nav>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import NavBar from './NavBar';
 import { Container, Theme, ThemePanel } from '@radix-ui/themes';
+import AuthProvider from './auth/provider';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -25,12 +26,14 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.variable}>
-        <Theme appearance='light' accentColor='purple'>
-          <NavBar />
-          <main className='p-5'>
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme appearance='light' accentColor='purple'>
+            <NavBar />
+            <main className='p-5'>
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
