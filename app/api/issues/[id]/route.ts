83de1fdefc1532d0b0dd0d40bfa0fef476454cc7
Mohamed Ali <immoe98@gmail.com>
2024-05-25 20:34:57 +0200

    Improve the user experience

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from 'next/server';
 import { issueSchema } from '@/app/validationSchemas';
 import prisma from '@/prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function PATCH(
   request: NextRequest,
