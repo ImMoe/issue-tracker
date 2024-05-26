c330f1e6c89812a33a4eaa973ed66b3f8a5b050c
Mohamed Ali <immoe98@gmail.com>
2024-05-26 23:23:45 +0200

    Build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Pagination from './components/Pagination';
 
 const Home = () => {
-  return <div>Home</div>;
+  return <Pagination itemCount={100} pageSize={10} currentPage={10} />;
 };
 
 export default Home;
