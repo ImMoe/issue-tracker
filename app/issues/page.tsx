9ab317720693ef527e78a340f1567273501d8f7b
Mohamed Ali <immoe98@gmail.com>
2024-05-26 22:43:24 +0200

    Sort Issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -24,10 +24,16 @@ const IssuesPage = async ({ searchParams }: SearchParams) => {
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: 'asc' }
+    : undefined;
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
   return (
     <div>
